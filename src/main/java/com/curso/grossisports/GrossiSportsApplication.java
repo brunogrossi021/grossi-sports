a92f839de4f58246829405bffd5c498daf80b735
brunogrossi021 <bhgobruno@gmail.com>
2026-08-31 17:54:14 -0300

    build: cria projeto Spring Boot

---
+++ src/main/java/com/curso/grossisports/GrossiSportsApplication.java
@@ -0,0 +1,13 @@
+package com.curso.grossisports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrossiSportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrossiSportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curso/grossisports/GrossiSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.grossisports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrossiSportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
